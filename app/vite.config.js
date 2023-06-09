c957afd43a38198098ebf9ef663867c9101531e6
yashweblife <yashweblife20@gmail.com>
2023-06-09 13:03:14 -0500

    add config file

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
     server:{
         port:3000,
         host:"0.0.0.0"
+    },
+    build:{
+        rollupOptions:{
+            input:{
+                index:"./index.html"
+            }
+        }
     }
 })
